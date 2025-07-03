c93b18a8199fd65af1a03cf5b622628d8c5fd3c6
Municksouza <munickfreitas@hotmail.com>
2025-07-03 14:51:27 -0600

    Precompiled assets for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,6 +94,7 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   config.middleware.use Rack::Deflater
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   
   config.public_file_server.headers = {
     'Cache-Control' => "public, max-age=#{2.days.to_i}"
