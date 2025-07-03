96ff08db52dc4553c5de244e16e1a960801923f8
Municksouza <munickfreitas@hotmail.com>
2025-07-03 16:01:53 -0600

    Force add precompiled assets for production

--- app/controllers/store_managers_area/receipts_controller.rb
+++ app/controllers/store_managers_area/receipts_controller.rb
@@ -2,6 +2,8 @@ require Rails.root.join("app/services/pdf_receipt_generator")
 module StoreManagersArea
   class ReceiptsController < ApplicationController
     before_action :authenticate_store_manager!
+    before_action :set_receipt
+
 
     def show
       @receipt = Receipt.find(params[:id])
@@ -16,6 +18,20 @@ module StoreManagersArea
           send_data pdf, filename: "Receipt_#{@receipt.id}.pdf", type: "application/pdf", disposition: "inline"
         end
       end
+    end
+
+    def download
+      if @receipt.pdf_file.attached?
+        redirect_to rails_blob_url(@receipt.pdf_file, disposition: "attachment", host: "https://getfood4less.ca")
+      else
+        redirect_to stores_receipts_path, alert: "PDF not available."
+      end
+    end
+
+    private
+
+    def set_receipt
+      @receipt = current_store.receipts.find(params[:id])
     end
   end
 end
