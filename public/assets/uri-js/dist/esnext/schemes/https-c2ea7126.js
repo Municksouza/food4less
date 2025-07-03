0e0a7c16f43db4189819eb4a261b8b3ee4d18f98
Municksouza <munickfreitas@hotmail.com>
2025-07-03 15:13:37 -0600

    Force add precompiled assets for production

---
+++ public/assets/uri-js/dist/esnext/schemes/https-c2ea7126.js
@@ -0,0 +1,9 @@
+import http from "./http";
+const handler = {
+    scheme: "https",
+    domainHost: http.domainHost,
+    parse: http.parse,
+    serialize: http.serialize
+};
+export default handler;
+//# sourceMappingURL=/assets/uri-js/dist/esnext/schemes/https-f4646194.js.map
