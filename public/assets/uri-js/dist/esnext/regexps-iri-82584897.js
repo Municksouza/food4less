3da7e99c39be82c1ca71ccf9caf1f6e26bd56508
Municksouza <munickfreitas@hotmail.com>
2025-07-04 15:54:18 -0600

    Fix assets issue and update styles

--- public/assets/uri-js/dist/esnext/regexps-iri-82584897.js
+++
@@ -1,3 +0,0 @@
-import { buildExps } from "./regexps-uri";
-export default buildExps(true);
-//# sourceMappingURL=/assets/uri-js/dist/esnext/regexps-iri-b7ba6661.js.map
--- public/assets/uri-js/dist/esnext/schemes/https-c2ea7126.js
+++
@@ -1,9 +0,0 @@
-import http from "./http";
-const handler = {
-    scheme: "https",
-    domainHost: http.domainHost,
-    parse: http.parse,
-    serialize: http.serialize
-};
-export default handler;
-//# sourceMappingURL=/assets/uri-js/dist/esnext/schemes/https-f4646194.js.map
